9c525f0395b193a42002f99e5502662abc49ca73
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-03-02 16:14:45 +0800

    Added the read lock in the get IP function
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- check/check.go
+++ check/check.go
@@ -1,3 +1,12 @@
+// Package check identifies if a given site is up or not.
+// Data is from https://isitup.org.
+//
+// Example:
+// check := check.New("google.com")
+//
+// check.IsUp()
+// check.IP()
+// check.Verbose()
 package check
 
 import (
@@ -33,11 +42,6 @@ type Check struct {
 	Req    *http.Request
 	Client *http.Client
 }
-
-// check.New("google.com")
-// check.IsUp()
-// check.IP()
-// check.Verbose()
 
 // New returns a pointer to check that defines the different methods to check a site.
 func New(site string) *Check {
@@ -97,6 +101,9 @@ func (c *Check) IsUp() bool {
 
 // IP returns the IP of the given site.
 func (c *Check) IP() string {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
 	body, err := getResponseBody(c.Client, c.Req)
 	if err != nil {
 		return ""
@@ -120,55 +127,3 @@ func getResponseBody(c *http.Client, req *http.Request) ([]byte, error) {
 
 	return ioutil.ReadAll(resp.Body)
 }
-
-// IsUp is our little helper to check if the site is up.
-// func IsUp(site string) bool {
-// 	reqURL := fmt.Sprintf("https://isitup.org/%s.json", site)
-//
-// 	req, err := http.NewRequest("GET", reqURL, nil)
-// 	if err != nil {
-// 		return false
-// 	}
-//
-// 	req.Header.Set("User-Agent", "https://github.com/BottleneckStudio/check")
-// 	var netTransport = &http.Transport{
-// 		Dial: (&net.Dialer{
-// 			Timeout: 5 * time.Second,
-// 		}).Dial,
-// 		TLSHandshakeTimeout: 5 * time.Second,
-// 	}
-//
-// 	var client = &http.Client{
-// 		Timeout:   time.Second * 10,
-// 		Transport: netTransport,
-// 	}
-//
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return false
-// 	}
-//
-// 	defer resp.Body.Close()
-//
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return false
-// 	}
-//
-// 	respo := Response{}
-//
-// 	err = json.Unmarshal(body, &respo)
-// 	if err != nil {
-// 		return false
-// 	}
-//
-// 	if respo.StatusCode == statusInvalidDomain {
-// 		return false
-// 	}
-//
-// 	if respo.StatusCode == statusDown {
-// 		return false
-// 	}
-//
-// 	return respo.StatusCode == statusSuccess
-// }
